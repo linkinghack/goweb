46625b1ee66bfbc5462ae9b8e33505fd01624c5b
LINKING-LAPTOP\linking <linkinghack@outlook.com>
2020-08-06 12:34:42 +0800

    Init go web app

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/linkinghack/goweb
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, Go web. requested: %s \n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":8088", nil)
+}
