04d8bfdc388031599c88cffbbbaf82dba4c72426
linkinghack <linkinghack@outlook.com>
2024-05-14 09:40:32 +0800

    Update Go version

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM golang:1.14-alpine as builder
+FROM golang:1.22.3-bullseye as builder
 WORKDIR /app
 COPY . /app
 RUN go build -o goweb
 
-FROM alpine
+FROM ubuntu
 COPY --from=builder /app/goweb /app/goweb
 EXPOSE 8088
 CMD ["/app/goweb"]
--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Req: %s \n", r.URL.Path)
-		fmt.Fprintf(w, "Hello, Go web. requested: %s \n", r.URL.Path)
+		fmt.Fprintf(w, "Hello, this is Go web! requested url: %s, Request time: %+v", r.URL.Path, time.Now())
 	})
 
-	http.ListenAndServe(":8088", nil)
+	fmt.Println("Start listening on port 8088..")
+	http.ListenAndServe("0.0.0.0:8088", nil)
 }
